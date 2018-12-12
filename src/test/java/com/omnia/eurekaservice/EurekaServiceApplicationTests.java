e32a2ca1c10af34be1861a8a10684b4f432634f3
Santos92 <AlexanderLundbergSantos@live.se>
2018-12-12 10:38:13 +0100

    added dockerfile

--- src/test/java/com/omnia/eurekaservice/EurekaServiceApplicationTests.java
+++ src/test/java/com/omnia/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,12 +1,14 @@
 package com.omnia.eurekaservice;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled:false"})
 public class EurekaServiceApplicationTests {
 
 	@Test
